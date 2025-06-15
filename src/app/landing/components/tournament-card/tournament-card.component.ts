713c29e6e270ac9c717e11d9a0edd6890dccb09e
feriveramar <fernandaa.riveramartinez@gmail.com>
2025-06-15 14:53:21 -0600

    update tourments held and players logic

--- src/app/landing/components/tournament-card/tournament-card.component.ts
+++ src/app/landing/components/tournament-card/tournament-card.component.ts
@@ -1,27 +1,44 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tournament-card',
   standalone: true,
   templateUrl: './tournament-card.component.html',
-  styleUrls: ['./tournament-card.component.css']
+  styleUrls: ['./tournament-card.component.css'],
+  imports: [CommonModule]
 })
 export class TournamentCardComponent {
   private static counter = 0;
-  public localImagePath: string = '';
+  private localImagePath: string = '';
 
+  // Alternar imágenes automáticamente
   @Input() set game(value: string) {
-    TournamentCardComponent.counter++;
-    const isEven = TournamentCardComponent.counter % 2 === 0;
+    if (value) {
+      TournamentCardComponent.counter++;
+      const isEven = TournamentCardComponent.counter % 2 === 0;
 
-    this.localImagePath = isEven
-      ? 'assets/images/imgValorant.png'
-      : 'assets/images/imgLoL.png';
+      this.localImagePath = isEven
+        ? 'assets/images/imgValorant.png'
+        : 'assets/images/imgLoL.png';
+    }
+    this._game = value;
+  }
+
+  private _game!: string;
+  get game(): string {
+    return this._game;
   }
 
   @Input() date!: string;
   @Input() prize!: string;
-  @Input() get image() {
+
+  @Input() get image(): string {
     return this.localImagePath;
   }
+
+  // Visor de estadísticas
+  @Input() showStats: boolean = false;
+  @Input() statsHeld?: number;
+  @Input() statsPlayers?: number;
 }
