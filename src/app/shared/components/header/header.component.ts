b2c4013079848561101969b8d42f78256bff753d
GioJCG <zS22004370@estudiantes.uv.mx>
2025-06-15 20:16:55 -0600

    creation of header and register

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,7 +11,13 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   menuOpen = false;
 
+  constructor(private router: Router) {}
+
   toggleMenu() {
-    this.menuOpen = !this.menuOpen
+    this.menuOpen = !this.menuOpen;
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
   }
 }
