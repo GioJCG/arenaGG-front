438bab1512f0ea0f1dce0be2705ba2400ff4488b
GioJCG <zS22004370@estudiantes.uv.mx>
2025-06-16 01:19:52 -0600

    update header routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeComponent } from './landing/home/home.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { PagesComponent } from './ranking/pages/pages.component';
 
 export const routes: Routes = [
   { path: '', loadComponent: () => import('./landing/home/home.component').then(c => c.HomeComponent)},
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent}
-
+  { path: 'register', component: RegisterComponent},
+  { path: 'ranking', component: PagesComponent}
 ];
